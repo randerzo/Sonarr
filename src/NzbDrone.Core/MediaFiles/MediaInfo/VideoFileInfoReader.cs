9963496ae89121ffadca652dfc339428c881f5ce
Taloth Saldono <Taloth@users.noreply.github.com>
2015-04-06 22:24:59 +0200

    Ensure mediainfo lib is only running on one thread.

--- src/NzbDrone.Core/MediaFiles/MediaInfo/VideoFileInfoReader.cs
+++ src/NzbDrone.Core/MediaFiles/MediaInfo/VideoFileInfoReader.cs
@@ -18,6 +18,7 @@ namespace NzbDrone.Core.MediaFiles.MediaInfo
         private readonly IDiskProvider _diskProvider;
         private readonly Logger _logger;
 
+        private static readonly object _syncRoot = new object();
 
         public VideoFileInfoReader(IDiskProvider diskProvider, Logger logger)
         {
@@ -33,95 +34,98 @@ namespace NzbDrone.Core.MediaFiles.MediaInfo
 
             MediaInfoLib.MediaInfo mediaInfo = null;
 
-            try
+            lock (_syncRoot)
             {
-                mediaInfo = new MediaInfoLib.MediaInfo();
-                _logger.Debug("Getting media info from {0}", filename);
-
-                mediaInfo.Option("ParseSpeed", "0.2");
-                int open = mediaInfo.Open(filename);
-
-                if (open != 0)
+                try
                 {
-                    int width;
-                    int height;
-                    int videoBitRate;
-                    int audioBitRate;
-                    int audioRuntime;
-                    int videoRuntime;
-                    int generalRuntime;
-                    int streamCount;
-                    int audioChannels;
-                    decimal videoFrameRate;
+                    mediaInfo = new MediaInfoLib.MediaInfo();
+                    _logger.Debug("Getting media info from {0}", filename);
 
-                    string subtitles = mediaInfo.Get(StreamKind.General, 0, "Text_Language_List");
-                    string scanType = mediaInfo.Get(StreamKind.Video, 0, "ScanType");
-                    Int32.TryParse(mediaInfo.Get(StreamKind.Video, 0, "Width"), out width);
-                    Int32.TryParse(mediaInfo.Get(StreamKind.Video, 0, "Height"), out height);
-                    Int32.TryParse(mediaInfo.Get(StreamKind.Video, 0, "BitRate"), out videoBitRate);
-                    Decimal.TryParse(mediaInfo.Get(StreamKind.Video, 0, "FrameRate"), NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out videoFrameRate);
+                    mediaInfo.Option("ParseSpeed", "0.2");
+                    int open = mediaInfo.Open(filename);
 
-                    //Runtime
-                    Int32.TryParse(mediaInfo.Get(StreamKind.Video, 0, "PlayTime"), out videoRuntime);
-                    Int32.TryParse(mediaInfo.Get(StreamKind.Audio, 0, "PlayTime"), out audioRuntime);
-                    Int32.TryParse(mediaInfo.Get(StreamKind.General, 0, "PlayTime"), out generalRuntime);
+                    if (open != 0)
+                    {
+                        int width;
+                        int height;
+                        int videoBitRate;
+                        int audioBitRate;
+                        int audioRuntime;
+                        int videoRuntime;
+                        int generalRuntime;
+                        int streamCount;
+                        int audioChannels;
+                        decimal videoFrameRate;
 
-                    string aBitRate = mediaInfo.Get(StreamKind.Audio, 0, "BitRate");
-                    int aBindex = aBitRate.IndexOf(" /", StringComparison.InvariantCultureIgnoreCase);
-                    if (aBindex > 0)
-                        aBitRate = aBitRate.Remove(aBindex);
+                        string subtitles = mediaInfo.Get(StreamKind.General, 0, "Text_Language_List");
+                        string scanType = mediaInfo.Get(StreamKind.Video, 0, "ScanType");
+                        Int32.TryParse(mediaInfo.Get(StreamKind.Video, 0, "Width"), out width);
+                        Int32.TryParse(mediaInfo.Get(StreamKind.Video, 0, "Height"), out height);
+                        Int32.TryParse(mediaInfo.Get(StreamKind.Video, 0, "BitRate"), out videoBitRate);
+                        Decimal.TryParse(mediaInfo.Get(StreamKind.Video, 0, "FrameRate"), NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out videoFrameRate);
 
-                    Int32.TryParse(aBitRate, out audioBitRate);
-                    Int32.TryParse(mediaInfo.Get(StreamKind.Audio, 0, "StreamCount"), out streamCount);
-                    
+                        //Runtime
+                        Int32.TryParse(mediaInfo.Get(StreamKind.Video, 0, "PlayTime"), out videoRuntime);
+                        Int32.TryParse(mediaInfo.Get(StreamKind.Audio, 0, "PlayTime"), out audioRuntime);
+                        Int32.TryParse(mediaInfo.Get(StreamKind.General, 0, "PlayTime"), out generalRuntime);
 
-                    string audioChannelsStr = mediaInfo.Get(StreamKind.Audio, 0, "Channel(s)");
-                    int aCindex = audioChannelsStr.IndexOf(" /", StringComparison.InvariantCultureIgnoreCase);
-                    if (aCindex > 0)
-                        audioChannelsStr = audioChannelsStr.Remove(aCindex);
+                        string aBitRate = mediaInfo.Get(StreamKind.Audio, 0, "BitRate");
+                        int aBindex = aBitRate.IndexOf(" /", StringComparison.InvariantCultureIgnoreCase);
+                        if (aBindex > 0)
+                            aBitRate = aBitRate.Remove(aBindex);
 
-                    string audioLanguages = mediaInfo.Get(StreamKind.General, 0, "Audio_Language_List");
-                    string audioProfile = mediaInfo.Get(StreamKind.Audio, 0, "Format_Profile");
+                        Int32.TryParse(aBitRate, out audioBitRate);
+                        Int32.TryParse(mediaInfo.Get(StreamKind.Audio, 0, "StreamCount"), out streamCount);
 
-                    int aPindex = audioProfile.IndexOf(" /", StringComparison.InvariantCultureIgnoreCase);
-                    if (aPindex > 0)
-                        audioProfile = audioProfile.Remove(aPindex);
 
-                    Int32.TryParse(audioChannelsStr, out audioChannels);
-                    var mediaInfoModel = new MediaInfoModel
-                                                {
-                                                    VideoCodec = mediaInfo.Get(StreamKind.Video, 0, "Codec/String"),
-                                                    VideoBitrate = videoBitRate,
-                                                    Height = height,
-                                                    Width = width,
-                                                    AudioFormat = mediaInfo.Get(StreamKind.Audio, 0, "Format"),
-                                                    AudioBitrate = audioBitRate,
-                                                    RunTime = GetBestRuntime(audioRuntime, videoRuntime, generalRuntime),
-                                                    AudioStreamCount = streamCount,
-                                                    AudioChannels = audioChannels,
-                                                    AudioProfile = audioProfile.Trim(),
-                                                    VideoFps = videoFrameRate,
-                                                    AudioLanguages = audioLanguages,
-                                                    Subtitles = subtitles,
-                                                    ScanType = scanType
-                                                };
+                        string audioChannelsStr = mediaInfo.Get(StreamKind.Audio, 0, "Channel(s)");
+                        int aCindex = audioChannelsStr.IndexOf(" /", StringComparison.InvariantCultureIgnoreCase);
+                        if (aCindex > 0)
+                            audioChannelsStr = audioChannelsStr.Remove(aCindex);
 
-                    return mediaInfoModel;
+                        string audioLanguages = mediaInfo.Get(StreamKind.General, 0, "Audio_Language_List");
+                        string audioProfile = mediaInfo.Get(StreamKind.Audio, 0, "Format_Profile");
+
+                        int aPindex = audioProfile.IndexOf(" /", StringComparison.InvariantCultureIgnoreCase);
+                        if (aPindex > 0)
+                            audioProfile = audioProfile.Remove(aPindex);
+
+                        Int32.TryParse(audioChannelsStr, out audioChannels);
+                        var mediaInfoModel = new MediaInfoModel
+                                                    {
+                                                        VideoCodec = mediaInfo.Get(StreamKind.Video, 0, "Codec/String"),
+                                                        VideoBitrate = videoBitRate,
+                                                        Height = height,
+                                                        Width = width,
+                                                        AudioFormat = mediaInfo.Get(StreamKind.Audio, 0, "Format"),
+                                                        AudioBitrate = audioBitRate,
+                                                        RunTime = GetBestRuntime(audioRuntime, videoRuntime, generalRuntime),
+                                                        AudioStreamCount = streamCount,
+                                                        AudioChannels = audioChannels,
+                                                        AudioProfile = audioProfile.Trim(),
+                                                        VideoFps = videoFrameRate,
+                                                        AudioLanguages = audioLanguages,
+                                                        Subtitles = subtitles,
+                                                        ScanType = scanType
+                                                    };
+
+                        return mediaInfoModel;
+                    }
                 }
-            }
-            catch (DllNotFoundException ex)
-            {
-                _logger.ErrorException("mediainfo is required but was not found", ex);
-            }
-            catch (Exception ex)
-            {
-                _logger.ErrorException("Unable to parse media info from file: " + filename, ex);
-            }
-            finally
-            {
-                if (mediaInfo != null)
+                catch (DllNotFoundException ex)
                 {
-                    mediaInfo.Close();
+                    _logger.ErrorException("mediainfo is required but was not found", ex);
+                }
+                catch (Exception ex)
+                {
+                    _logger.ErrorException("Unable to parse media info from file: " + filename, ex);
+                }
+                finally
+                {
+                    if (mediaInfo != null)
+                    {
+                        mediaInfo.Close();
+                    }
                 }
             }
 
